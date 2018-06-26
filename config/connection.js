b2724d0bb465d7674d7d3b1884257ef349b9750c
brendankim93 <31469167+brendankim93@users.noreply.github.com>
2018-06-26 15:15:33 -0700

    back to normal

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
 var mysql = require("mysql");
 
-//mysql://b3c3f8962912c9:7c39ddb1@us-cdbr-iron-east-04.cleardb.net/heroku_597f109a91bac38?reconnect=true
 
 var connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-04.cleardb.net",
-  user: "b3c3f8962912c9",
-  password: "7c39ddb1",
-  database: "heroku_597f109a91bac38",
-  port: 3306
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db",
+  port: 8889
 });
 
 connection.connect(function(err) {
