ecac8c19b345b2a158adacf2fc092f703af73ea6
brendankim93 <31469167+brendankim93@users.noreply.github.com>
2018-06-26 02:30:06 -0700

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
 
+//mysql://b3c3f8962912c9:7c39ddb1@us-cdbr-iron-east-04.cleardb.net/heroku_597f109a91bac38?reconnect=true
+
 var connection = mysql.createConnection({
   port: 3900,
   host: "us-cdbr-iron-east-04.cleardb.net",
   user: "b3c3f8962912c9",
   password: "7c39ddb1",
   database: "heroku_597f109a91bac38",
-  port: 8889
+  port: 3306
 });
 
 connection.connect(function(err) {
